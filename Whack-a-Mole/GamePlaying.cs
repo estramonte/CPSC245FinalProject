b5b3a817dc25520735e01b80beec864e9db6766b
estramonte <89673824+estramonte@users.noreply.github.com>
2022-12-14 08:18:20 -0800

    Update GamePlaying.cs

--- Whack-a-Mole/GamePlaying.cs
+++ Whack-a-Mole/GamePlaying.cs
@@ -19,9 +19,10 @@ public class GamePlaying : MonoBehaviour
     private int RandomMoleNumber;
     private int temp = 10;
     private int totalMoles = 0;
-    private float timeFromEachMolePop = .2f;
+    private float timeFromEachMolePop = 1f;
     private float timeWaitForResults = 1f;
-
+    
+    //Controls the moles coming up.
     private IEnumerator MolesUpTillTimeIsOut()
     {
         while(timer.timeOnGame>0){
@@ -30,14 +31,16 @@ public class GamePlaying : MonoBehaviour
         }
         StartCoroutine(WaitForResults());
     }
-
+    
+    //Pauses the game for a moment after the timer runs out so it doesn't feel rushed.
     private IEnumerator WaitForResults()
     {
         yield return new WaitForSeconds(timeWaitForResults);
         ui.ShowMoleNumber(totalMoles);
         game.StartResults();
     }
-
+    
+    //Randomly generates between the 9 moles
     private int RandomMoleNumberGenerator()
     {
         RandomMoleNumber = Random.Range(0,9);
@@ -50,18 +53,20 @@ public class GamePlaying : MonoBehaviour
         return RandomMoleNumber;
     }
 
+   //if mole not hit and not currently generated
+   //start coroutine for a random time for the mole to come up
     private void randomMoleComingUp()
     {
-        //if mole not hit and not currently generated
-        //start coroutine for a random time for the mole to come up
         StartCoroutine(moleList[RandomMoleNumberGenerator()].ShowHide(new Vector2(0f, -0.25f), new Vector2(0f,1f)));
     }
 
+    //Starts the molesuptilltimeisout coroutine.
     public void GoMoles()
     {
         StartCoroutine(MolesUpTillTimeIsOut());
     }
-
+    
+    //Reset function
     public void Reset()
     {
         totalMoles = 0;
