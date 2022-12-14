65a5bdd60c2e89dee79e942d63f3a363b322bb64
estramonte <89673824+estramonte@users.noreply.github.com>
2022-12-14 08:24:45 -0800

    Update Sounds.cs

--- Whack-a-Mole/Sounds.cs
+++ Whack-a-Mole/Sounds.cs
@@ -1,3 +1,11 @@
+//Marissa Estramonte and Quillan Gee
+//2405291, 2404244 
+//estramonte@chapman.edu, qgee@chapman.edu 
+//245-01
+//Final Project: Whack-a-mole
+
+//Managed all the audioclips. 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
