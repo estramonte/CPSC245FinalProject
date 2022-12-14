5f20777ce6db91fa7412925a833d91f7b2f28a7d
estramonte <89673824+estramonte@users.noreply.github.com>
2022-12-14 08:26:54 -0800

    Update Timer.cs

--- Whack-a-Mole/Timer.cs
+++ Whack-a-Mole/Timer.cs
@@ -1,3 +1,11 @@
+//Marissa Estramonte and Quillan Gee
+//2405291, 2404244 
+//estramonte@chapman.edu, qgee@chapman.edu 
+//245-01
+//Final Project: Whack-a-mole
+
+//Controls the in-game Timer.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
